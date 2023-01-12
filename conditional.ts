f31b750759e54b2d421f4146057974c204577a6a
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-13 00:06:16 +0900

    conditional

--- conditional.ts
+++ conditional.ts
@@ -2,40 +2,38 @@ let score = 770;
 
 if (score >= 700) {
   console.log("Good");
-}
-else if (score >= 600){
-    console.log("Fair");
-}
-else {
-    console.log("Fail");
+} else if (score >= 600) {
+  console.log("Fair");
+} else {
+  console.log("Fail");
 }
 
 enum Category {
-    VVIP,
-    VIP,
-    REGULAR
+  VVIP,
+  VIP,
+  REGULAR,
 }
 
 let mycategory: Category = Category.VIP;
 
 function classification(category: Category): void {
-    switch (category) {
-        case Category.VVIP:
-            console.log("VVIP");
-            break;
-        case Category.VIP:
-            console.log("VIP");
-            break;
-        case Category.REGULAR:
-            console.log("REGULAR");
-            break;
-        default:
-            console.log("Unknown");
-    }
+  switch (category) {
+    case Category.VVIP:
+      console.log("VVIP");
+      break;
+    case Category.VIP:
+      console.log("VIP");
+      break;
+    case Category.REGULAR:
+      console.log("REGULAR");
+      break;
+    default:
+      console.log("Unknown");
+  }
 }
 
 classification(mycategory);
 
 let isMember: boolean = true;
 let fee = isMember ? 5 : 10;
-console.log(fee)
+console.log(fee);
---
+++ function.ts
@@ -0,0 +1,8 @@
+function sayHello(firstName: string, lastName: string): void {
+    let name = `${firstName} ${lastName}`;
+    console.log(`Hello, ${name}`);
+}
+
+sayHello("Tom", "Jerry");
+
+// optional parameter
